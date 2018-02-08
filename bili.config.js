73b9eb24c4b37dab926902578dd9ed4b1e444782
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-02-08 17:19:33 +0700

    rebuild

--- bili.config.js
+++ bili.config.js
@@ -1,6 +1,10 @@
+const vue = require('rollup-plugin-vue')
+
 module.exports = {
   banner: true,
   format: ['umd-min'],
-  plugins: ['vue'],
-  extract: false
+  css: true,
+  plugins: [
+    vue({ css: true })
+  ]
 }
